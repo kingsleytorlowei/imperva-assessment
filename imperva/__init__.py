f54e89844e8a01b68e3dbc88f7616f4c3de3700a
Kingsley Torlowei <kingsleytorlowei@gmail.com>
2021-08-07 00:46:39 -0400

    fix: added trailing slash to views

--- imperva/__init__.py
+++ imperva/__init__.py
@@ -17,7 +17,7 @@ def create_app():
     def register_api(view, endpoint, url, pk='id', pk_type='int'):
         view_func = view.as_view(endpoint)
         app.add_url_rule(url, defaults={pk: None},view_func=view_func, methods=['GET'])
-        app.add_url_rule(f'{url}<{pk_type}:{pk}>', view_func=view_func, methods=['GET'])
+        app.add_url_rule(f'{url}<{pk_type}:{pk}>/', view_func=view_func, methods=['GET'])
   
     register_api(CustomersAPI, 'customer_api', '/customers/', pk='customer_id')
     register_api(FilmsAPI, 'film_api', '/films/', pk='film_id')
