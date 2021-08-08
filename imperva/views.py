ace0570d996b6c69f065ac25dbefc8de14d35163
Kingsley Torlowei <kingsleytorlowei@gmail.com>
2021-08-08 00:57:41 -0400

    feat: cleaned up views for customers and films api

--- imperva/views.py
+++ imperva/views.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 import json
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 from .db import DBClient
 from .utils import create_customer_data, create_film_data
@@ -15,14 +16,14 @@ class CustomersAPI(MethodView, DBClient):
     def get(self, customer_id):
         cursor = self.db_client['customers']
         if customer_id is None:
-            limit = int(request.args.get('limit'))
+            limit = request.args.get('limit')
             if limit is None or limit == '':
-                limit = 0
-            offset = int(request.args.get('offset'))
+                limit = 10
+            offset = request.args.get('offset')
             if offset is None or offset == '':
                 offset = 0
             customers_placeholder = []
-            all_customers = cursor.find({}).skip(offset).limit(limit)
+            all_customers = cursor.find({}).skip(int(offset)).limit(int(limit))
             for customer in all_customers:
                 customer_json_data = {}
                 customer_json_data['id'] = customer['_id']
@@ -47,15 +48,45 @@ class FilmsAPI(MethodView, DBClient):
         self.db_client = self.db_client().imperva_db
 
     def get(self, film_id):
-        cursor = self.db_client['films']
+        film_cursor = self.db_client['films']
         if film_id is None:
-            film_data = cursor.find_one({'_id': film_id})
-            return film_data
+            limit = request.args.get('limit')
+            if limit is None or limit == '':
+                limit = 10
+            offset = request.args.get('offset')
+            if offset is None or offset == '':
+                offset = 0
+            films_placeholder = []
+            all_films = film_cursor.find({}).skip(int(offset)).limit(int(limit))
+            for film in all_films:
+                films_json_data = create_film_data(film)
+                films_placeholder.append(films_json_data)
+            return Response(response=json.dumps(films_placeholder),
+                    status=200,
+                    mimetype='application/json')
         else:
-            film_data = cursor.find_one({'_id':film_id})
+            customer_cursor = self.db_client['customers']
+            film_data = film_cursor.find_one({'_id':film_id})
             if film_data == None:
                 raise Exception('film with id not found')
-            cleaned_rental_data = create_film_data(film_data)
-            return Response(response=json.dumps(cleaned_rental_data),
+
+            customer_list = []
+            all_customers = customer_cursor.find({})
+            for customer in all_customers:
+                customer_rentals = customer['Rentals']
+                for rental in customer_rentals:
+                    customer_json_rentals = {}
+                    if rental['filmId'] == film_id:
+                        customer_json_rentals['id'] = customer['_id']
+                        customer_json_rentals['name'] = customer['First Name'].title() + ' ' + customer['Last Name'].title()
+                        customer_list.append(customer_json_rentals)
+
+            film_response_data = create_film_data(film_data)
+            film_response_data['film_length'] = film_data['Length']
+            film_response_data['replacement_cost'] = film_data['Replacement Cost']
+            film_response_data['special_features'] = film_data['Special Features']
+            film_response_data['customers'] = customer_list
+
+            return Response(response=json.dumps(film_response_data),
                     status=200,
                     mimetype='application/json')
