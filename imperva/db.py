2c7b89c50bca8383e46b405172806551cf8bfbe6
Kingsley Torlowei <kingsleytorlowei@gmail.com>
2021-08-08 00:56:33 -0400

    chore: added docstring to DBClient

--- imperva/db.py
+++ imperva/db.py
@@ -6,6 +6,7 @@ from pymongo import MongoClient
 load_dotenv()
 
 class DBClient:
+    ''' Class which abstracts the connection to mongo db '''
     def db_client(self):
         mongo_uri = os.getenv("MONGO_URI", None)
         if mongo_uri == None:
