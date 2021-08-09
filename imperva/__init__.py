60725b7512816de1b4204fe2289918995c5a1485
Kingsley Torlowei <kingsleytorlowei@gmail.com>
2021-08-09 17:09:43 -0400

    fix: added base_url

--- imperva/__init__.py
+++ imperva/__init__.py
@@ -14,14 +14,14 @@ def create_app():
         SECRET_KEY='dev'
     )
     app.config.update(config)
-
+    api_base_url= 'api/'
     def register_api(view, endpoint, url, pk='id', pk_type='int'):
         view_func = view.as_view(endpoint)
         app.add_url_rule(url, defaults={pk: None},view_func=view_func, methods=['GET'])
         app.add_url_rule(f'{url}<{pk_type}:{pk}>/', view_func=view_func, methods=['GET'])
   
-    register_api(CustomersAPI, 'customer_api', '/customers/', pk='customer_id')
-    register_api(FilmsAPI, 'film_api', '/films/', pk='film_id')
+    register_api(CustomersAPI, f'{api_base_url}customer_api', '/customers/', pk='customer_id')
+    register_api(FilmsAPI, f'{api_base_url}film_api', '/films/', pk='film_id')
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
